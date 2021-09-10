4d721fa7892219035a0f7dcc6b6c7b146723f63f
Ruslan Musanipov <abachok.000@mail.ru>
2021-09-10 09:11:14 +0300

    br2

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //branch 1
+        //branch 2
     }
 }
